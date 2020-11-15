40efdc993795ba68fde624cb1017a54c6be9e655
Eugene <Eugene Hung@Eugene_Hung.ggec.gd>
2020-11-15 13:33:34 +0800

    20201115

---
+++ apps/applications/sink/qcc512x_qcc302x/common/build_id_str.c
@@ -0,0 +1,6 @@
+#include <hydra_types.h>
+
+/** This file will be re-generated on each build */
+
+const uint8 build_id_string[] = "QTIL ADK 2020-11-10 20:24:24 @1605011064";
+const uint32 build_id_number = 1605011064UL;
